e9750269fe5c00137acf6aee9564d51e154c28d2
Chris Roberson <chrisronline@gmail.com>
2014-07-22 14:33:21 -0400

    v1.0.3. Support for timeout maxCount and a trigger

--- README.md
+++ README.md
@@ -1,12 +1,49 @@
-angular-focus-first-field
+angular-focus-field-field
 =====================
 
 AngularJS directive that focuses the first empty input
 
 Usage
 -----
+
+###Basic
+
+Inspects the input children of the form and focuses the first one that has no value
+
     <ng-form name="myForm" focus-first-field>
     </ng-form>
+
+###Trigger
+
+Define a condition that when true will cause the focus code to execute
+
+#### .js
+    angular.module('myApp')
+        .controller('myCtrl', function($scope, $timeout) {
+            $scope.myTrigger = false;
+            $timeout(function() {
+                $scope.myTrigger = true;
+            });
+        });
+
+
+#### .html
+    <ng-form name="myForm"
+        focus-first-field
+        focus-first-field-trigger="myTrigger">
+    </ng-form>
+
+###Timeout Attempts
+
+In the situation where the input fields are not available immediately (like generated
+by another directive), specify the number of timeouts (or digest cycles) it will take
+for those fields to be ready (defaults to 5 attempts)
+
+    <ng-form name="myForm"
+        focus-first-field
+        focus-first-field-timeout-attempts="2">
+    </ng-form>
+
 
 Installation
 ---------
--- angular-focus-first-field.js
+++ angular-focus-first-field.js
@@ -2,14 +2,17 @@
   'use strict';
 
   angular.module('angular-focus-first-field', [])
-    .directive('focusFirstField', function($timeout) {
+    .directive('focusFirstField', function($timeout, $parse) {
       return {
         restrict: 'A',
         link: function($scope, $element, $attrs) {
-          $timeout(function() {
+          var inputs = $element.find('input');
+          var attemptsLeft = parseInt($attrs.focusFirstFieldTimeoutAttempts);
+          if (isNaN(attemptsLeft)) attemptsLeft = 5;
+
+          function go() {
             var stop = false;
             var first = null;
-            var inputs = $element.find('input');
             angular.forEach(inputs, function(input) {
               if (!first) {
                 first = input;
@@ -26,7 +29,35 @@
             if (!stop && first) {
               first.focus();
             }
-          });
+          }
+
+          function attempt() {
+            $timeout(function() {
+              inputs = $element.find('input');
+              --attemptsLeft;
+              check();
+            });
+          }
+
+          function check() {
+            if (inputs.length) {
+              go();
+            }
+            else {
+              attempt();
+            }
+          }
+
+          if ($parse($attrs.focusFirstFieldTrigger).assign) {
+            $scope.$watch($attrs.focusFirstFieldTrigger, function(newValue) {
+              if (newValue) {
+                check();
+              }
+            });
+          }
+          else {
+            $timeout(check);
+          }
         }
       };
     });
